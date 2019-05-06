486afc66d768f8cafa7b8cc06255ab761f222522
John Shaskin <guitarjohn@gmail.com>
2019-05-05 22:09:18 -0600

    pass ingredients to checkout page.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1,
-    },
+    ingredients: null,
   };
+
+  componentDidMount() {
+    this.loadIngredientsFromSearch();
+  }
+
+  componentDidUpdate() {
+    this.loadIngredientsFromSearch();
+  }
+
+  loadIngredientsFromSearch() {
+    const query = new URLSearchParams(this.props.history.location.search);
+    if (this.state.ingredients === null) {
+      let stuff = {}
+      for (var entry of query.entries()) {
+          const key = entry[0];
+          stuff[key] = Number.parseInt(entry[1]);
+      }
+      this.setState({ ingredients: stuff });
+    }
+  }
 
   checkoutCanceledHandler = () => {
     this.props.history.goBack();
@@ -21,12 +37,18 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
+    let checkoutSummary = <Spinner />;
+    if (this.state.ingredients) {
+      checkoutSummary = (
         <CheckoutSummary
           ingredients={this.state.ingredients}
           checkoutCanceled={this.checkoutCanceledHandler}
           checkoutContinue={this.checkoutContinueHandler} />
+      )
+    }
+    return (
+      <div>
+        {checkoutSummary}
       </div>
     )
   }
