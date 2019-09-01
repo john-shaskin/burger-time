208ff0f470ce0b0de0e6503478b548bbc326dd3a
John Shaskin <guitarjohn@gmail.com>
2019-08-31 21:38:01 -0600

    Add a Logout component that will issue the logout action, and redirect to root path

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
 export { orderBurger, orderInit, fetchOrders } from './order';
-export { auth } from './auth';
+export { auth, logout } from './auth';
