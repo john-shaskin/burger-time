b5b540f7d505ebe7b4417733391545ec7d8f6d92
John Shaskin <guitarjohn@gmail.com>
2019-09-10 23:03:31 -0600

    Add more tests for NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -5,11 +5,26 @@ import Adapter from 'enzyme-adapter-react-16';
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it('should render two <NavigationItem /> elements when user is not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('should render three <NavigationItem /> elements when user is authenticated', () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should render a logout <NavigationItem /> when user is authenticated', () => {
+    wrapper.setProps({ isAuth: true});
+    expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toBeTruthy();
   });
 });
