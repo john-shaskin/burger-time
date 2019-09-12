369213f5fe7bccf9cc9c41f4994a4401ce77bbd7
John Shaskin <guitarjohn@gmail.com>
2019-09-11 22:57:09 -0600

    Define static site bucket with deployment from build folder

--- infra/lib/static-site-stack.ts
+++ infra/lib/static-site-stack.ts
@@ -1,9 +1,39 @@
 import cdk = require('@aws-cdk/core');
+import route53 = require('@aws-cdk/aws-route53');
+import s3 = require('@aws-cdk/aws-s3');
+import s3Deployment = require('@aws-cdk/aws-s3-deployment');
+
+export interface StaticSiteStackProps extends cdk.StackProps {
+  hostedZoneId: string,
+  staticAssetsPath?: string,
+}
 
 export class StaticSiteStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: StaticSiteStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * Get external resources
+     */
+    const hostedZone = route53.HostedZone.fromHostedZoneId(this, 'HostedZone', props.hostedZoneId);
+    // TODO:
+    // S3 bucket with public access, and HTTPS support
+    // S3 deployment of build folder + .env file
+    // Cloudfront distribution (eventually)
+    // Route53 recordset
+    const recordSetName = 'burger-time';
+    // const siteName = `${recordSetName}.${hostedZone.zoneName}`;
+
+    const bucket = new s3.Bucket(this, 'SiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      bucketName: `${recordSetName}-static-content`
+    });
+
+    const staticAssetsPath = props.staticAssetsPath || '../build/';
+    new s3Deployment.BucketDeployment(this, 'SiteDeployment', {
+      destinationBucket: bucket,
+      source: s3Deployment.Source.asset(staticAssetsPath),
+    } as s3Deployment.BucketDeploymentProps);
   }
 }
